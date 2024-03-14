6198977de89442568f72d4a00954054e0fd46236
s7manth <syalamarty002@gmail.com>
2024-03-14 16:36:56 +0800

    add python script to download model repositories from hugging face

---
+++ utils/download-model.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+from dotenv import load_dotenv
+from huggingface_hub import snapshot_download
+
+load_dotenv(".env")
+
+MODELS_DIR = os.getenv("MODELS_DIR")
+HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+
+if not MODELS_DIR:
+    print("ERROR: MODELS_DIR environment variable not defined")
+    exit(1)
+
+if not HUGGING_FACE_TOKEN:
+    print("ERROR: HUGGING_FACE_TOKEN environment variable not defined")
+    exit(1)
+
+model_id = sys.argv[1]
+
+cache_dir = f"{MODELS_DIR}/.cache"
+local_dir = f"{MODELS_DIR}/{model_id}"
+
+snapshot_path = snapshot_download(
+    repo_id=model_id,
+    local_dir=local_dir,
+    cache_dir=cache_dir
+)
+
+print(f"SUCCESS: model downloaded at {MODELS_DIR}")
